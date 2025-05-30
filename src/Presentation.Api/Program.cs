c793b2356726f61e802a94941e5eb20a094750c3
mikepowtech <mike.warnes@powtechnology.com>
2025-05-30 16:09:43 +0100

    Remove rg-metronvu build config; add unified app config
    This commit removes the entire build and deployment configuration for the .NET Core application in `rg-metronvu.yml`, including all related environment variables and job steps.
    
    A new build configuration for a unified app is introduced in `unifiedapp.yml`, with updated job names and environment variables reflecting the new application context, including Azure Web App details and API management settings.
    
    Refactor CI/CD and enhance API management setup
    
    Updated `rg-metronvu.yml` to streamline deployment steps and remove unnecessary environment variables. Modified `unifiedapp.yml` to include new environment variables and re-enable Open API Specification generation. Introduced a `Startup` class in `Program.cs` for better service configuration, added necessary namespaces, and updated middleware for improved security and functionality.

--- src/Presentation.Api/Program.cs
+++ src/Presentation.Api/Program.cs
@@ -1,14 +1,66 @@
 using Application;
+using Application.Identity;
+using Infrastructure;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System.Text;
-using Infrastructure;
-using Application.Identity;
+
 namespace Api
 {
+    public class Startup(IConfiguration configuration, IWebHostEnvironment enviroment)
+    {
+        public IConfiguration Configuration { get; } = configuration;
+        public IWebHostEnvironment Environment { get; } = enviroment;
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //service.AddSingleton<ISomeOtherService, SomeOtherService>();
+            services.AddSwaggerGen(config =>
+            {
+                config.SwaggerDoc("v1", new OpenApiInfo() { Title = "App Api", Version = "v1" });
+                config.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "bearer"
+                });
+                config.AddSecurityRequirement(
+                    new OpenApiSecurityRequirement{
+                        {
+                            new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Type=ReferenceType.SecurityScheme,
+                                    Id="Bearer"
+                                }
+                            },
+                            Array.Empty<string>()
+                        }
+                    });
+            });
+
+            services.AddMvc();
+            //etc
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            if (Environment.IsDevelopment() || Environment.IsEnvironment("Test"))
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+        }
+    }
+
     public class Program
     {
         public static async Task Main(string[] args)
@@ -64,44 +116,49 @@ namespace Api
                 });
             });
 
-            builder.Services.AddSwaggerGen(config =>
-            {
-                config.SwaggerDoc("v1", new OpenApiInfo() { Title = "App Api", Version = "v1" });
-                config.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-                {
-                    In = ParameterLocation.Header,
-                    Description = "Please enter token",
-                    Name = "Authorization",
-                    Type = SecuritySchemeType.Http,
-                    BearerFormat = "JWT",
-                    Scheme = "bearer"
-                });
-                config.AddSecurityRequirement(
-                    new OpenApiSecurityRequirement{
-                        {
-                            new OpenApiSecurityScheme
-                            {
-                                Reference = new OpenApiReference
-                                {
-                                    Type=ReferenceType.SecurityScheme,
-                                    Id="Bearer"
-                                }
-                            },
-                            Array.Empty<string>()
-                        }
-                    });
-            });
+            var startup = new Startup(builder.Configuration, builder.Environment);
+            startup.ConfigureServices(builder.Services);
+
+            //builder.Services.AddSwaggerGen(config =>
+            //{
+            //    config.SwaggerDoc("v1", new OpenApiInfo() { Title = "App Api", Version = "v1" });
+            //    config.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            //    {
+            //        In = ParameterLocation.Header,
+            //        Description = "Please enter token",
+            //        Name = "Authorization",
+            //        Type = SecuritySchemeType.Http,
+            //        BearerFormat = "JWT",
+            //        Scheme = "bearer"
+            //    });
+            //    config.AddSecurityRequirement(
+            //        new OpenApiSecurityRequirement{
+            //            {
+            //                new OpenApiSecurityScheme
+            //                {
+            //                    Reference = new OpenApiReference
+            //                    {
+            //                        Type=ReferenceType.SecurityScheme,
+            //                        Id="Bearer"
+            //                    }
+            //                },
+            //                Array.Empty<string>()
+            //            }
+            //        });
+            //});
 
             var app = builder.Build();
+
+            startup.Configure(app);
+
             if (app.Environment.IsDevelopment() || app.Environment.IsEnvironment("Test"))
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
                 using var scope = app.Services.CreateScope();
                 var initialiser = scope.ServiceProvider.GetRequiredService<ApplicationDbContextInitialiser>();
                 await initialiser.InitialiseAsync();
                 await initialiser.SeedAsync();
             }
+
             app.UseHttpsRedirection();
             app.UseCors("webAppRequests");
             app.UseAuthentication();
